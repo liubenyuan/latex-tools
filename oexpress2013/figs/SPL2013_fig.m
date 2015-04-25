829e55d4e03bd4c17a1e3413feb32062486f9be8
liubenyuan <liubenyuan@gmail.com>
2015-04-25 19:52:36 +0000

    add optic express 2013 templates

---
+++ oexpress2013/figs/SPL2013_fig.m
@@ -0,0 +1,10 @@
+% convert dot in figs to small-lines
+
+fixPSlinestyle('Figure2.eps','Figure2.eps');
+fixPSlinestyle('Figure3.eps','Figure3.eps');
+fixPSlinestyle('Figure4.eps','Figure4.eps');
+fixPSlinestyle('Figure5.eps','Figure5.eps');
+fixPSlinestyle('Figure6.eps','Figure6.eps');
+% fixPSlinestyle('Figure7.eps','Figure7.eps');
+% fixPSlinestyle('Figure8.eps','Figure8.eps');
+disp('done');
