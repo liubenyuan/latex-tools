86eba97d0dadcb9a6eaa463a460b2e9033535d02
Liu Benyuan <liubenyuan@gmail.com>
2020-08-03 13:29:45 +0800

    update prefs from 7.2.20

--- ipe-config/prefs.lua
+++ ipe-config/prefs.lua
@@ -4,7 +4,7 @@
 --[[
 
     This file is part of the extensible drawing editor Ipe.
-    Copyright (C) 1993-2016  Otfried Cheong
+    Copyright (c) 1993-2020 Otfried Cheong
 
     Ipe is free software; you can redistribute it and/or modify it
     under the terms of the GNU General Public License as published by
@@ -37,6 +37,24 @@ prefs = {}
 -- directory.
 prefs.styles = { "doctor_xe" }
 
+-- The TeX engine for new documents
+-- Possible values are "default", "pdftex", "xetex", and "luatex"
+prefs.tex_engine = "xetex"
+
+-- What to display on the "View" and "Page" button
+-- Standard:
+prefs.view_button_prefix = "View "
+prefs.page_button_prefix = "Page "
+-- Concise:
+-- prefs.view_button_prefix = "V "
+-- prefs.page_button_prefix = "P "
+-- Expert:
+-- prefs.view_button_prefix = ""
+-- prefs.page_button_prefix = ""
+
+-- URL for a cloud service that provides Latex compilation
+prefs.latex_service_url = "https://latexonline.cc"
+
 -- Interval for autosaving in seconds
 -- set to nil to disable autosaving
 prefs.autosave_interval = 600 -- 10 minutes
@@ -45,6 +63,9 @@ prefs.autosave_interval = 600 -- 10 minutes
 -- can contain '%s' for the filename of the current file
 -- can use 'home' for the user's home directory
 -- prefs.autosave_filename = home .. "/autosave.ipe"
+-- On Windows, config.ipedrive gives you the root of the drive
+-- where Ipe is installed.
+-- prefs.autosave_filename = config.ipedrive .. "/autosave.ipe"
 if config.platform == "win" then
   prefs.autosave_filename = config.documents .. "\\%s.autosave"
 else
@@ -54,6 +75,10 @@ end
 -- Should Ipe show the Developer menu
 -- (only useful if you develop ipelets or want to customize Ipe)
 prefs.developer = false
+
+-- Should Ipe terminate when the last window was closed?
+-- (Currently only on OS X)
+prefs.terminate_on_close = true
 
 -- Enable continuous spell-checking in text fields by default?
 -- (Currently only on OS X)
@@ -65,9 +90,17 @@ prefs.spell_check = true
 if os.getenv("EDITOR") then
   prefs.external_editor = os.getenv("EDITOR") .. " %s"
 else
-  -- prefs.external_editor = "pluma %s"
+  if config.platform == "win" then
+    prefs.external_editor = "notepad.exe %s"
+  elseif config.platform == "apple" then
+    prefs.external_editor = "open -W -n -e %s"
+  else
+    -- prefs.external_editor = "xed %s"
+  end
+  -- For Emacs users:
   -- prefs.external_editor = "emacsclient %s"
-  prefs.external_editor = nil
+  -- if you use Visual studio, you make like the following:
+  -- prefs.external_editor = "code --wait %s"
 end
 
 -- The name of the temporary textfile where Ipe will store text to be
@@ -85,16 +118,32 @@ else
   prefs.delete_key = "\127"
 end
 
--- Where the tools should be put (left or right)
+-- Where the tools should be placed (left or right)
 -- and which ones should be displayed on start-up
--- (left/right currently only used with Qt toolkit)
-prefs.tools = {
+-- tools_placement is only used with Qt toolkit
+prefs.tools_placement = {
   properties = "left",
   bookmarks = "right",
   notes = "right",
-  layers = "left",
-  hidden = { "bookmarks", "notes" }
+  layers = "left"
 }
+prefs.tools_visible = {
+  properties = true,
+  bookmarks = false,
+  notes = false,
+  layers = true
+}
+
+-- on OSX, specify a minimum width for the properties and layer panels (in pixels)
+-- to ensure that the width of the canvas does not change while you step through your document
+-- if you change "view_button_prefix" and "page_button_prefix", you can make this smaller
+prefs.osx_properties_width = 200.0
+
+-- on Windows, initial ordering of the toolbars
+prefs.win_toolbar_order = { "snap", "grid", "angle", "edit", "mode" }
+
+-- on Windows, keep edit toolbar minimal?
+prefs.win_mini_edit = false
 
 -- Should LaTeX be run automatically every time text has changed?
 prefs.auto_run_latex = false
@@ -105,11 +154,20 @@ prefs.auto_external_editor = nil
 -- Should closing the external editor immediately close the editing dialog?
 prefs.editor_closes_dialog = nil
 
+-- If the user interface is too small, scale it here (Windows and Qt only)
+-- 100 is the original size, 150 is 50% larger
+prefs.ui_scaling = 100
+-- The following scales the size of the toolbar buttons
+prefs.toolbar_scaling = 100
+
 -- Minimum size of dialog window for creating/editing text objects
 prefs.editor_size = { 0, 0 }
 
+-- Width of Notes and Bookmarks tools (Windows only)
+prefs.width_notes_bookmarks = 200
+
 -- Size of main window at startup
-prefs.window_size = { 1080, 600 }
+prefs.window_size = { 1160, 720 }
 
 -- Size of Latex error message window
 prefs.latexlog_size = { 800, 600 }
@@ -119,6 +177,16 @@ prefs.page_sorter_size = { 960, 600 }
 
 -- Width of page thumbnails (height is computed automatically)
 prefs.thumbnail_width = 200
+
+-- The laser pointer
+prefs.laser_pointer = {
+  color = { r = 1.0, g = 0.0, b = 0.0 }, -- red
+  pen = 6.0,
+  radius = 7.0,
+}
+
+-- tension of cardinal splines
+prefs.spline_tension = 0.5
 
 -- Canvas customization:
 prefs.canvas_style = {
@@ -139,8 +207,6 @@ prefs.canvas_style = {
   -- e.g. try this: thin_step = 2, thick_step = 5
 }
 
--- Should the grid be visible when Ipe starts? (true or false)
-prefs.grid_visible = true
 
 -- Snap modes active when Ipe starts:
 prefs.snap = { vertex = false,
@@ -149,17 +215,53 @@ prefs.snap = { vertex = false,
 	       intersection = false,
 	       grid = true,
 	       angle = false,
+	       custom = false,
 	       autoangle = false }
 
--- Initial grid size and angle
-prefs.grid_size = 16     -- points
-prefs.angle_size = 45    -- degrees
+-- UI settings when Ipe starts
+prefs.initial = {
+  grid_size = 16,     -- points
+  angle_size = 45,    -- degrees
+  grid_visible = true,
+}
+
+-- Attributes set when Ipe starts
+prefs.initial_attributes = {
+  pathmode = "stroked",
+  stroke = "black",
+  fill = "white",
+  pen = "normal",
+  dashstyle = "normal",
+  farrowshape = "arrow/normal(spx)",
+  rarrowshape = "arrow/normal(spx)",
+  farrowsize = "normal",
+  rarrowsize = "normal",
+  farrow = false,
+  rarrow = false,
+  symbolsize = "normal",
+  textsize = "normal",
+  textstyle = "normal",
+  transformabletext = false,
+  splinetype = "bspline",
+  horizontalalignment = "left",
+  verticalalignment = "baseline",
+  pinned = "none",
+  transformations = "affine",
+  linejoin = "normal",
+  linecap = "normal",
+  fillrule = "normal",
+  markshape = "mark/disk(sx)",
+  tiling = "normal",
+  gradient = "normal",
+  opacity = "opaque",
+  strokeopacity = "opaque",
+}
 
 -- Resolution settings
 
 -- When you select "normal size" in Ipe, this is the pixels
 -- per inch you want to see.
-prefs.normal_resolution = 72
+prefs.normal_resolution = 96
 
 -- Maximum distance in pixels selecting/snapping
 prefs.select_distance = 36
@@ -170,19 +272,39 @@ prefs.close_distance = 48
 
 -- Zoom factors, minimal and maximal possible zoom
 prefs.zoom_factor = 1.3
--- when zooming by scroll wheel or
--- by scroll gesture on OS X
-if config.toolkit == "cocoa" then
-  prefs.wheel_zoom_factor = 1.1
-  prefs.wheel_other_factor = 1.3 -- used on some external mice
-else
-  prefs.wheel_zoom_factor = 1.3
-end
 prefs.min_zoom = 0.1
 prefs.max_zoom = 100
 
--- Should newly created text be transformable by default?
-prefs.text_transformable = false
+----------------------------------------------------------------------
+
+-- The following settings determine what the scroll wheel on a mouse or a
+-- two-finger pan gesture on a touchpad/trackpad  do.
+
+-- There are two sets of settings: "trackpad_scroll" applies to trackpads under OSX,
+-- "scroll" applies in all other cases (including external mice in OSX).
+
+-- When pan is true, the gesture moves (pans) the canvas around, and
+-- you can control the speed of panning with pixel_per_degree.
+-- The direction of the movement is determined by direction.x and direction.y (1 or -1).
+-- When pan is false, the vertical scroll movement is used to change the zoom level.
+-- You can control the zoom speed using zoom_per_degree.
+-- If you want to reverse the direction of zooming, change
+-- zoom_per_degree to something smaller than one, e.g. 0.9827
+
+prefs.scroll = { pan = false,
+	         pixel_per_degree = 2,
+		 zoom_per_degree = 1.0176,
+		 direction = { x=1, y=1 } }
+prefs.trackpad_scroll = { pan = true,
+	              	  pixel_per_degree = 2,
+		 	  zoom_per_degree = 1.0176,
+			  direction = { x=1, y=1 } }
+
+----------------------------------------------------------------------
+
+-- How close do two vertices need to be to be considered the same
+-- in the "Join paths" operation?
+prefs.join_threshold = 1e-6
 
 -- If set to true, then whenever the user edits the title of a page,
 -- the check box "section: use title" is checked automatically.
@@ -191,18 +313,14 @@ prefs.automatic_use_title = false
 -- How to start browser to show Ipe manual:
 -- (not used with Windows or Cocoa toolkit)
 
--- On Linux, there are various options depending on your Desktop:
+prefs.browser = "xdg-open %s"
+-- On Linux, there are some other options you can try if xdg-open
+-- doesn't work on your desktop:
 -- Gnome 2: gnome-open
--- prefs.browser = "gnome-open %s"
 -- Gnome 3: gvfs-open
-prefs.browser = "gvfs-open %s"
 -- KDE: kde-open
--- prefs.browser = "kde-open %s"
 -- XFCE: exo-open
--- prefs.browser = "exo-open %s"
--- "sensible-browser" starts a new browser
--- (which is not nice, ideally we'd like a tab in an open browser window)
--- prefs.browser = "sensible-browser %s &"
+-- Ubuntu/Debian: sensible-browser
 
 -- How to start onscreen keyboard
 if config.platform == "win" then
@@ -214,10 +332,74 @@ else
   prefs.keyboard = nil
 end
 
--- Do not highlight current selection in "ink" drawing mode?
-prefs.no_ink_highlight = false
 -- Disable all snap modes in "ink" drawing mode?
-prefs.no_ink_snap = false
+prefs.no_ink_snap = true
+
+----------------------------------------------------------------------
+--
+-- The following settings can be used to tune the use of the pen in
+-- ink mode.  It seems digitizers differ a lot, here you can adapt Ipe
+-- to yours.
+--
+-- If your digitizer generates lots of pen move events, set
+-- ink_min_movement to avoid making thousands of tiny ink segments
+-- (start with a value like 0.2).
+--
+-- If there is a lot of noise in pen recognition, try setting
+-- ink_smoothing to a positive value k (smoothing is then performed
+-- over 2k+1 samples).  Try 3, for instance.
+--
+-- After a complete ink stroke has been drawn, Ipe simplifies it to
+-- remove excess vertices, by computing a path with the minimal number
+-- of vertices that differs by at most ink_tolerance.
+--
+-- Ink strokes that are very short (like a dot on an i) tend to appear
+-- too small. Use ink_dot_length and ink_dot_wider to enlarge them.
+--
+-- Finally, you can set ink_spline to have your ink strokes converted
+-- to a spline (rather than a polygonal curve).
+--
+----------------------------------------------------------------------
+
+-- Minimal distance for movement to update ink stroke
+prefs.ink_min_movement = 0
+
+-- To smooth ink strokes, perform sliding average over how many samples?
+prefs.ink_smoothing = 0
+
+-- When ink stroke is simplified, what is the tolerance?
+prefs.ink_tolerance = 0.3
+
+-- Threshold for considering an ink stroke a tap with little movement:
+prefs.ink_dot_length = 10.0
+
+-- When user taps the screen in ink mode with no or little movement,
+-- how much should the pen width be enlarged?
+-- nil means use current pen width
+prefs.ink_dot_wider = 2.0
+
+-- Turn ink strokes into splines?
+prefs.ink_spline = false
+
+----------------------------------------------------------------------
+
+-- Auto-exporting when document is being saved
+-- If this is set, then every time you save in xml format, say to
+-- mydoc.ipe, the same document is EXPORTED to mydoc.format,
+-- for each of the enabled formats.
+-- Careful: if the file exists, it is silently overwritten.
+-- The exported files cannot be opened by Ipe (they are exported copies).
+-- Export to eps, png, svg use the first view of the first page only.
+
+-- possible formats are "pdf", "eps", "svg", "png"
+-- e.g. prefs.auto_export = { "pdf", "eps" }
+prefs.auto_export = { }
+
+-- the pixels per inch resolution to use for PNG export
+prefs.auto_export_resolution = 100
+
+----------------------------------------------------------------------
+
 -- Extended properties menu, perhaps useful for tablets:
 prefs.tablet_menu = false
 
